b4bb1ec950c53abe5b1d4dd12846a2f79b1f3905
joelavero <joelavero@gmail.com>
2024-04-22 11:21:23 -0300

    add swagger

--- api/index.js
+++ api/index.js
@@ -1,14 +1,21 @@
 const express = require("express");
-const config = require("./config");
-const user = require("./components/user/network");
 const bodyParser = require("body-parser");
+
+const swaggerUI = require("swagger-ui-express");
+
+const config = require("../config");
+const user = require("./components/user/network");
+
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
 
+const swaggerDoc = require("./openapi.json");
+
 // Routes
 app.use("/api/user", user);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 app.listen(config.api.port, () =>
   console.log(`Listening on port ${config.api.port}`)
